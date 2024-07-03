be94f9ed0753458b3de75b500ba0abf4eeb84cc6
linossh <joaodevlino@gmail.com>
2024-07-03 23:29:53 +0100

    Initial Commit

---
+++ emoji-picker/emoji_pickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  emoji_pickerApp.swift
+//  emoji-picker
+//
+//  Created by João Lino on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct emoji_pickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
